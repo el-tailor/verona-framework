4bd8e21a2dee8c19665ad0e8945153614de435e8
Tailor Mavoungou <TailorMavoungou@outlook.com>
2025-02-17 00:01:20 +0100

    correction bug

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 if (php_sapi_name() == "cli-server") {
-    $file = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $file = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     if (is_file($file)) return false;
 }
 
